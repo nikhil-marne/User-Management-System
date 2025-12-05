88ab714c90a5398fa51db490e81793489af6dea9
Nikhil Marne <marne.nikhil@outlook.com>
2025-12-05 07:59:15 +0530

    Added file upload & session handling functionality, implementation remains

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import path from "node:path";
+import multer from "multer";
+import session from "express-session";
 import connectDB from "./Models/connectDB.js";
 import {
-  adduser,
   deleteUserById,
   getAllUsers,
   getUserByid,
@@ -14,6 +15,13 @@ const PORT = 5800;
 const BASE_DIR = path.resolve(".");
 const app = express();
 
+// SETTING UP MULTER
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) =>
+    cb(null, path.resolve(BASE_DIR, "fileUploads")),
+  filename: (req, file, cb) => cb(null, file.originalname),
+});
+const multerApp = multer({ storage: storageConfig });
 // CONNECTING DATABASE
 await connectDB();
 
@@ -22,6 +30,15 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(BASE_DIR, "public")));
+app.use(
+  session({
+    secret: "pratiksha",
+    resave: false, // resave session
+    rolling: true,
+    cookie: { maxAge: 1000 * 60 * 60 },
+    saveUninitialized: false,
+  })
+);
 
 // APP ROUTE START
 
@@ -55,6 +72,24 @@ app
       );
     }
   });
+
+app.get("/file", (req, res) => {
+  res.render("fileForm");
+});
+
+app.post("/file/upload", multerApp.single("file"), (req, res) => {
+  res.send({ message: req.file });
+});
+
+app.get("/user/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/user/profile", (req, res) => {
+  res.render("profile");
+  req.session.data = req.body;
+  console.log(req.session.data);
+});
 
 // ADDING LISTENER TO APP
 app.listen(PORT, () => {
